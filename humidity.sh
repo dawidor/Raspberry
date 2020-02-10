9247efab7654bc89c4f8e6cffc4e04efa6fa332f
Rafal Dawidowicz <dr4aa@hotmail.com>
2020-02-10 22:20:58 +0100

    pms

--- humidity.sh
+++ humidity.sh
@@ -11,6 +11,8 @@ done < <(python /home/pi/dev/Raspberry/humidity.py)
 
 #echo ${array[@]}
 
+echo "L: $timestamp_"
+
 curl -i -XPOST "http://192.168.1.155:8086/write?db=datacenters" --data-binary "humiditi,home=piltza43 value=${array[1]} $timestamp_"
 
 curl -i -XPOST "http://192.168.1.155:8086/write?db=datacenters" --data-binary "temp2,home=piltza43 value=${array[0]} $timestamp_"
